c0aab07fc4ff4e79911f7e2a2e4b37d3a8562f09
Zafer Balkan <zafer@zaferbalkan.com>
2023-04-29 22:24:40 +0300

    Pdf support (#41)
    * Implemented PDF scan
    
    * Updated README
    
    * Used PDF BytesIO instead of Bytes

---
+++ src/pdf.py
@@ -0,0 +1,25 @@
+from io import BufferedReader, BytesIO, StringIO
+from typing import Optional, Union
+
+from pdfminer.high_level import extract_text
+
+
+class Pdf:
+
+    __file:Optional[str | BytesIO] = None
+
+    def __init__(self, file:str | BytesIO) -> None:
+            self.__file=file
+
+
+    def get_text(self) ->str:
+        in_file : Optional[Union[BufferedReader, BytesIO]] = None
+        if isinstance(self.__file,str):
+            in_file = open(self.__file, "rb")
+        elif isinstance(self.__file,BytesIO):
+                in_file = self.__file
+
+        if in_file is None:
+            return ''
+
+        return extract_text(in_file)
