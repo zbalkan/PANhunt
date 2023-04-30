96a3a652a7875f915190c86b6baeb519914606f7
Zafer Balkan <zafer@zaferbalkan.com>
2023-04-30 16:10:43 +0300

    Used Powershell instead of batch

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-:: Update paths accordingly, like "<path to virtual env>/Lib/site-packages"
-set path = ".venv/Lib/site-packages"
-
-pyinstaller src/panhunt.py -F --clean -i dionach.ico --paths=%path% --hidden-import=appdirs --hidden-import=pyparsing
